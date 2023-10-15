ac7f44a9e3c5fa089e9adfdbedf4fffbcc4e06e5
MohammadHasan Akbari <jarqvi.jarqvi@gmail.com>
2023-10-15 06:56:47 +0330

    feat: add admin category template

--- www/html/utils/pdo.php
+++ www/html/utils/pdo.php
@@ -7,12 +7,12 @@ try {
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, 
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ
     );
-    $pdo = new PDO("mysql:host=mysql;dbname=blog-php", "root", "root", $options);
+    $pdo = new PDO("mysql:host=mysql;dbname=blog", "root", "root", $options);
 
-    echo json_encode(array(
-        "status" => "success",
-        "message" => "PDO connected to mysql"
-    ));
+    // echo json_encode(array(
+    //     "status" => "success",
+    //     "message" => "PDO connected to mysql"
+    // ));
     return $pdo;
 } catch (PDOException $e) {
     echo "error" . $e->getMessage();
